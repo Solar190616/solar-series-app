f15448d0ab95a87bdda91cdaf4d0e9be964d3a7c
Solar190616 <sherchansajjan22@gmail.com>
2025-07-31 19:16:06 +0900

    Delete solar_series_app/auth.py

--- solar_series_app/auth.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def check_login(username, password):
-    try:
-        with open("users.json", "r") as f:
-            users = json.load(f)
-        return username in users and users[username] == password
-    except:
-        return False

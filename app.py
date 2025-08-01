070bbe12ac1bc55b10b0dce7aaca35ab9d4d027f
Solar190616 <sherchansajjan22@gmail.com>
2025-08-01 16:17:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import math
 import pandas as pd
 
-from auth import check_login, create_user, update_password
-from db   import init_db, save_module, load_modules, delete_module
-
 # — hide the entire top-right toolbar (share, fork, GitHub icon) —
 st.markdown(
     """
